3959ab8fac3924aac517310c669090bdb647d37a
Jeff Ward <jeffwardcoding@gmail.com>
2015-11-25 12:29:00 -0800

    Wired up Angular and file structure

---
+++ js/MemoryGame.ctrl.js
@@ -0,0 +1,7 @@
+angular.module('app').controller('MemoryGameController', function($scope, $timeout){
+    
+    
+    
+    
+    
+}); //End Controller
---
+++ js/app.js
@@ -0,0 +1,5 @@
+angular.module('app', ['ngMaterial']).config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+        .primaryPalette('blue-grey');
+});
+
